1c348a8c569840a9a6ffa6e0e7c317d84e9acdac
saitejakuruva <saitejaplr7@gmail.com>
2019-09-12 12:32:03 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header'
+import About from './About'
+
 
 class App extends Component{
   render(){
     return(
       <div>
  <Header />
+ <About />
       </div>
     )
   }
